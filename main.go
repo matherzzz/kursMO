16661155015bb391f90343327ffa84df2f5acbbe
mather <ivan.gopin@mail.ru>
2023-01-12 19:15:28 +0500

    Gauss

--- main.go
+++ main.go
@@ -46,70 +46,120 @@ func fTruth() [stepT][stepX]float64 {
 	return f
 }
 
-func diagonal(A [][]float64) []float64 {
-	mtr := MakeRatio(A)
-	answer := make([]float64, 0)
-	answer = append(answer, mtr[1][len(A)-1])
-	for i := 2; i <= len(A); i++ {
-		answer = append(answer, mtr[0][len(A)-i]*answer[i-2]+mtr[1][len(A)-i])
+func Gauss(A [stepX - 2][stepX - 2]float64, b [stepX - 2]float64) [stepX - 2]float64 {
+	var answer, buff1 [stepX - 2]float64
+	var sum, buff2 float64
+	for k := 0; k < len(A); k++ {
+		max := -100000.0
+		num := -1
+		// ищем ведущий элемент
+		for i := k; i < len(A); i++ {
+			if math.Abs(A[i][k]) > max {
+				max = math.Abs(A[i][k])
+				num = i
+			}
+		}
+		// меняем местами строки
+		if num != k {
+			buff2 = b[num]
+			buff1 = A[num]
+			b[num] = b[k]
+			A[num] = A[k]
+			A[k] = buff1
+			b[k] = buff2
+		}
+		// делим строку на ведущий элемент
+		for i := k + 1; i < len(A); i++ {
+			A[k][i] /= A[k][k]
+		}
+		b[k] /= A[k][k]
+		A[k][k] = 1
+		// вычитаем строку из остальных
+		for i := k + 1; i < len(A); i++ {
+			for j := k + 1; j < len(A); j++ {
+				A[i][j] -= A[k][j] * A[i][k]
+			}
+			b[i] -= b[k] * A[i][k]
+			A[i][k] = 0
+		}
+	}
+	// обратный ход
+	for i := len(A) - 1; i >= 0; i-- {
+		sum = b[i]
+		for j := len(A) - 1; j > i; j-- {
+			sum -= A[i][j] * answer[i+1]
+		}
+		answer[i] = sum
 	}
 	return answer
-}
-
-func MakeRatio(A [][]float64) [][]float64 {
-	mtr := make([][]float64, 0)
-	var v float64
-	alpha0 := make([]float64, 0)
-	beta := make([]float64, 0)
-	alpha0 = append(alpha0, -A[0][2]/A[0][1])
-	beta = append(beta, A[0][3]/A[0][1])
-	for i := 1; i < len(A); i++ {
-		v = -A[i][1] - A[i][0]*alpha0[i-1]
-		if i == len(A)-1 {
-			alpha0 = append(alpha0, 0)
-		} else {
-			alpha0 = append(alpha0, A[i][2]/v)
-		}
-		beta = append(beta, (A[i][0]*beta[i-1]-A[i][3])/v)
-	}
-	mtr = append(mtr, alpha0)
-	mtr = append(mtr, beta)
-	return mtr
 }
 
 func implicitStraight(f [stepT][stepX]float64) {
 	U := make([][]float64, 2)
 	x := make([]float64, 0)
 	t := make([]float64, 0)
+	var buff float64
 	for j := 0; j < stepX; j++ {
 		U[0] = append(U[0], phi(h*float64(j)))
 		U[1] = append(U[1], U[0][j]+tau*eta(h*float64(j)))
 	}
-	buff := 1 / (math.Gamma(1-alpha) * (alpha*alpha - 3*alpha + 2) * math.Pow(h, 1+alpha))
-	for i := 2; i < stepT; i++ {
+	coefficient := 1 / (math.Gamma(1-alpha) * h * h)
+	for n := 2; n < stepT; n++ {
 		U = append(U, make([]float64, 0))
-		U[i] = append(U[i], 0)
+		U[n] = append(U[n], 0)
 		//создание и заполнение матрицы
-		A := make([][]float64, 0)
-		for j := 1; j < stepX-1; j++ {
-			A = append(A, make([]float64, 4))
-			if j > 1 {
-				A[j-1][0] = -buff * (alpha - 1)
+		var A [stepX - 2][stepX - 2]float64
+		var columnFreeMembers [stepX - 2]float64
+		for i := 1; i < stepX-1; i++ {
+			A[i-1][i-1] += 1 / (tau * tau)
+			columnFreeMembers[i-1] += (2*U[n-1][i]-U[n-2][i])/(tau*tau) + f[n][i]
+			if i < stepX-2 {
+				for k := 1; k <= i+1; k++ {
+					columnFreeMembers[i-1] += (math.Pow(h*float64(i+1-k), 1-alpha) - math.Pow(h*float64(i+2-k), 1-alpha)) / (alpha - 1)
+					buff = coefficient * (math.Pow(h*float64(i+1-k), 1-alpha)*h*(alpha-1+float64(k-2-i)) + math.Pow(h*float64(i+2-k), 2-alpha)) / (h * (alpha*alpha - 3*alpha + 2))
+					if k != i+1 {
+						A[i-1][k-1] -= buff
+						if k-1 > 0 {
+							A[i-1][k-2] += buff
+						} else {
+							columnFreeMembers[i-1] -= buff
+						}
+					} else {
+						A[i-1][k-1] += buff
+						columnFreeMembers[i-1] += buff
+					}
+				}
 			}
-			A[j-1][1] = 1/(tau*tau) + alpha*buff
-			A[j-1][3] = (2*U[i-1][j]-U[i-2][j])/(tau*tau) + f[i][j]
-			if j < stepX-2 {
-				A[j-1][2] = -buff
+			for k := 1; k <= i; k++ {
+				columnFreeMembers[i-1] -= 2 * (math.Pow(h*float64(i-k), 1-alpha) - math.Pow(h*float64(i+1-k), 1-alpha)) / (alpha - 1)
+				buff = 2 * coefficient * (math.Pow(h*float64(i-k), 1-alpha)*h*(alpha-1+float64(k-1-i)) + math.Pow(h*float64(i+1-k), 2-alpha)) / (h * (alpha*alpha - 3*alpha + 2))
+				A[i-1][k-1] += buff
+				if k-1 > 0 {
+					A[i-1][k-2] -= buff
+				} else {
+					columnFreeMembers[i-1] += buff
+				}
+			}
+			if i > 1 {
+				for k := 1; k <= i-1; k++ {
+					columnFreeMembers[i-1] -= 2 * (math.Pow(h*float64(i-1-k), 1-alpha) - math.Pow(h*float64(i-k), 1-alpha)) / (alpha - 1)
+					buff = 2 * coefficient * (math.Pow(h*float64(i-1-k), 1-alpha)*h*(alpha-1+float64(k-i)) + math.Pow(h*float64(i-k), 2-alpha)) / (h * (alpha*alpha - 3*alpha + 2))
+					A[i-1][k-1] += buff
+					if k-1 > 0 {
+						A[i-1][k-2] -= buff
+					} else {
+						columnFreeMembers[i-1] += buff
+					}
+				}
 			}
 		}
-		A[0][0], A[len(A)-1][2] = 0, 0
 		//решаем систему
-		answer := diagonal(A)
+		answer := Gauss(A, columnFreeMembers)
 		//заполняем слой
-		for j := 1; j <= len(answer); j++ {
-			U[i] = append(U[i], answer[len(answer)-j])
+		for j := 0; j < len(answer); j++ {
+			U[n] = append(U[n], answer[j])
 		}
-		U[i] = append(U[i], 0)
+		U[n] = append(U[n], 0)
 	}
 	for i := 0; i < stepX; i++ {
 		x = append(x, float64(i)*h)
@@ -128,7 +178,8 @@ func main() {
 	f := fTruth()
 	fmt.Print(math.Gamma(1 - alpha))
 	implicitStraight(f)
-	//mtr := [stepX - 2][4]float64{{0, 2, 3, 1}, {1, 2, 3, 1}, {1, 2, 3, 1}, {1, 2, 0, 1}}
-	//answer := diagonal(mtr)
+	//mtr := [stepX - 2][stepX - 2]float64{{2, 1, 0}, {1, 2, 4}, {0, 5, 2}}
+	//f := [stepX - 2]float64{1, 1, 1}
+	//answer := Gauss(mtr, f)
 	//fmt.Print(answer)
 }
