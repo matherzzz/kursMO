575244d617d25eb773f5b074fb35b6a2b7ed57ac
mather <ivan.gopin@mail.ru>
2023-01-20 18:43:12 +0500

    small optimizations

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/borgishmorg/go3dplot"
 	"math"
 )
@@ -12,24 +13,25 @@ const (
 	l     = 1
 	tau   = T / float64(stepT-1)
 	h     = l / float64(stepX-1)
-	alpha = 0.5
+	alpha = 0.99
 )
 
 func phi(x float64) float64 {
-	return (1 - x) * x
+	return (1 - x) * x * x
 }
 
 func eta(x float64) float64 {
-	return (1 - x) * x
+	return (1 - x) * x * x
 }
 
 func impact(f *[stepT][stepX]float64) {
 	var x, t float64
-	for i := 2; i < stepT; i++ {
-		for j := 1; j < stepX-1; j++ {
+	for i := 0; i < stepT; i++ {
+		for j := 1; j < stepX; j++ {
 			t = float64(i) * tau
 			x = float64(j) * h
-			f[i][j] = math.Exp(t) * ((1-x)*x - (math.Pow(x, -alpha)*(alpha+2*x-1)/(alpha-1))/math.Gamma(1-alpha))
+			f[i][j] = math.Exp(t) * ((1-x)*x*x + 2*math.Pow(x, 1-alpha)*(alpha+3*x-2)/(math.Gamma(1-alpha)*(alpha*alpha-3*alpha+2)))
+			//f[i][j] = x * t * 0
 		}
 	}
 }
@@ -81,12 +83,12 @@ func gauss(matrix *[stepX - 2][stepX - 2]float64, columnFreeMembers *[stepX - 2]
 
 func implicitStraight(f *[stepT][stepX]float64) [][]float64 {
 	U := make([][]float64, 2)
-	var buffer1 float64
+	var buffer float64
 	for j := 0; j < stepX; j++ {
 		U[0] = append(U[0], phi(h*float64(j)))
 		U[1] = append(U[1], U[0][j]+tau*eta(h*float64(j)))
 	}
-	coefficient := 1 / (math.Gamma(1-alpha) * h * h)
+	coefficient := 1 / (math.Gamma(1-alpha) * math.Pow(h, alpha+1) * (alpha*alpha - 3*alpha + 2))
 	for n := 2; n < stepT; n++ {
 		U = append(U, make([]float64, 0))
 		U[n] = append(U[n], 0)
@@ -94,29 +96,29 @@ func implicitStraight(f *[stepT][stepX]float64) [][]float64 {
 		var matrix [stepX - 2][stepX - 2]float64
 		var columnFreeMembers [stepX - 2]float64
 		for i := 1; i < stepX-1; i++ {
-			matrix[i-1][i-1] += 1 / (tau * tau)
-			columnFreeMembers[i-1] += (2*U[n-1][i]-U[n-2][i])/(tau*tau) + f[n][i]
+			matrix[i-1][i-1] = 1 / (tau * tau)
+			columnFreeMembers[i-1] = (2*U[n-1][i]-U[n-2][i])/(tau*tau) + f[n][i]
 			for k := 1; k <= i+1; k++ {
-				buffer1 = coefficient * (math.Pow(h*float64(i+1-k), 1-alpha)*h*(alpha-3+float64(k-i)) + math.Pow(h*float64(i+2-k), 2-alpha)) / (h * (alpha*alpha - 3*alpha + 2))
-				if i < stepX-2 || k != i+1 {
-					matrix[i-1][k-1] -= buffer1
+				buffer = coefficient * (math.Pow(float64(i+1-k), 1-alpha)*(alpha-3+float64(k-i)) + math.Pow(float64(i+2-k), 2-alpha))
+				if i != stepX-2 || k != i+1 {
+					matrix[i-1][k-1] -= buffer
 				}
 				if k-1 > 0 {
-					matrix[i-1][k-2] += buffer1 - coefficient*(math.Pow(h*float64(i+1-k), 1-alpha)-math.Pow(h*float64(i+2-k), 1-alpha))/(alpha-1)
+					matrix[i-1][k-2] += buffer - coefficient*(alpha-2)*(math.Pow(float64(i+1-k), 1-alpha)-math.Pow(float64(i+2-k), 1-alpha))
 				}
 			}
 			for k := 1; k <= i; k++ {
-				buffer1 = 2 * coefficient * (math.Pow(h*float64(i-k), 1-alpha)*h*(alpha-2+float64(k-i)) + math.Pow(h*float64(i+1-k), 2-alpha)) / (h * (alpha*alpha - 3*alpha + 2))
-				matrix[i-1][k-1] += buffer1
+				buffer = 2 * coefficient * (math.Pow(float64(i-k), 1-alpha)*(alpha-2+float64(k-i)) + math.Pow(float64(i+1-k), 2-alpha))
+				matrix[i-1][k-1] += buffer
 				if k-1 > 0 {
-					matrix[i-1][k-2] -= buffer1 - 2*coefficient*(math.Pow(h*float64(i-k), 1-alpha)-math.Pow(h*float64(i+1-k), 1-alpha))/(alpha-1)
+					matrix[i-1][k-2] -= buffer - 2*coefficient*(alpha-2)*(math.Pow(float64(i-k), 1-alpha)-math.Pow(float64(i+1-k), 1-alpha))
 				}
 			}
 			for k := 1; k <= i-1; k++ {
-				buffer1 = coefficient * (math.Pow(h*float64(i-1-k), 1-alpha)*h*(alpha-1+float64(k-i)) + math.Pow(h*float64(i-k), 2-alpha)) / (h * (alpha*alpha - 3*alpha + 2))
-				matrix[i-1][k-1] -= buffer1
+				buffer = coefficient * (math.Pow(float64(i-1-k), 1-alpha)*(alpha-1+float64(k-i)) + math.Pow(float64(i-k), 2-alpha))
+				matrix[i-1][k-1] -= buffer
 				if k-1 > 0 {
-					matrix[i-1][k-2] += buffer1 - coefficient*(math.Pow(h*float64(i-1-k), 1-alpha)-math.Pow(h*float64(i-k), 1-alpha))/(alpha-1)
+					matrix[i-1][k-2] += buffer - coefficient*(alpha-2)*(math.Pow(float64(i-1-k), 1-alpha)-math.Pow(float64(i-k), 1-alpha))
 				}
 			}
 		}
@@ -133,6 +135,8 @@ func implicitStraight(f *[stepT][stepX]float64) [][]float64 {
 
 func main() {
 	var f [stepT][stepX]float64
+	max := 0.0
+	maxI, maxJ := 0, 0
 	impact(&f)
 	x := make([]float64, 0)
 	t := make([]float64, 0)
@@ -143,23 +147,29 @@ func main() {
 	for i := 0; i < stepT; i++ {
 		t = append(t, float64(i)*tau)
 	}
-	//UErr := make([][]float64, 0)
-	//UT := make([][]float64, 0)
-	//for i := 0; i < stepT; i++ {
-	//	UT = append(UT, make([]float64, 0))
-	//	for j := 0; j < stepX; j++ {
-	//		UT[i] = append(UT[i], math.Exp(float64(i)*tau)*((1-float64(j)*h)*float64(j)*h))
-	//	}
-	//}
-	//for i := 0; i < stepT; i++ {
-	//	UErr = append(UErr, make([]float64, 0))
-	//	for j := 0; j < stepX; j++ {
-	//		UErr[i] = append(UErr[i], math.Abs(UT[i][j]-U[i][j]))
-	//	}
-	//}
+	UErr := make([][]float64, 0)
+	UT := make([][]float64, 0)
+	for i := 0; i < stepT; i++ {
+		UT = append(UT, make([]float64, 0))
+		for j := 0; j < stepX; j++ {
+			UT[i] = append(UT[i], math.Exp(float64(i)*tau)*(1-float64(j)*h)*float64(j)*h*float64(j)*h)
+		}
+	}
+	for i := 0; i < stepT; i++ {
+		UErr = append(UErr, make([]float64, 0))
+		for j := 0; j < stepX; j++ {
+			UErr[i] = append(UErr[i], math.Abs(UT[i][j]-U[i][j]))
+			if UErr[i][j] > max {
+				max = UErr[i][j]
+				maxI, maxJ = i, j
+			}
+		}
+	}
+	fmt.Println(max)
+	fmt.Print(maxI, maxJ)
 	// отрисовка графика функции U(x,t)
 	drawer := go3dplot.GetGnuplotDrawer()
-	err := drawer.Draw(x, t, U, "example")
+	err := drawer.Draw(x, t, UErr, "example2")
 	if err != nil {
 		return
 	}
